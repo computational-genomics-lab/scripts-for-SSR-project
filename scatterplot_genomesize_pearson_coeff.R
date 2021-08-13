dff3a41f9cf381894ac1c2fd56302f18b761c184
Aditya-Upa <83017903+Aditya-Upa@users.noreply.github.com>
2021-08-13 15:37:42 +0530

    Add files via upload

---
+++ scatterplot_genomesize_pearson_coeff.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(ggpubr)
+
+ggscatter(ssr_mean_genome_size, x = "Genome_size", y = "mean",
+          add = "reg.line", conf.int = TRUE,
+          cor.coef = TRUE,  cor.coef.size = 4,  cor.method = "pearson",
+          xlab = "Genome sizes", ylab = "Means of SSR lengths") +
+  scale_y_continuous (breaks=seq(0,25,by=0.5)) +
+  ggtitle("means of ssr lengths vs genome sizes (with Pearson's Correlation
+  Coefficient added) ")
+
