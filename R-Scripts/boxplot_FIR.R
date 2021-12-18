66fa35e110c9813e3f4ea39713ff6f4ca3340985
Aditya-Upa <83017903+Aditya-Upa@users.noreply.github.com>
2021-12-18 20:05:16 +0530

    Add files via upload

---
+++ R-Scripts/boxplot_FIR.R
@@ -0,0 +1,16 @@
+#the script creates a boxplot of all the column values in dataset 
+
+library(ggplot2)
+
+fill <- "gold1"
+line <- "goldenrod2"
+
+ggplot(stack(X2speed), aes(x = ind, y = values)) +
+  geom_boxplot(width=0.25, fill = fill, colour = line) +
+  theme(axis.text=element_text(size=15, face="bold"), axis.title=element_text(size=17,face="bold"), axis.text.x = element_text( angle = 90, hjust=1))+
+  labs(x = " ") +
+  scale_y_continuous(breaks=seq(0,14000,by=1000), limits = c(0, 14000))
+
+ 
+
+
